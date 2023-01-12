53c367e120839680252ea53b0d054e0e457744f4
GenVenom <shalindevkota22@gmail.com>
2023-01-12 19:59:35 +0545

    Added follow / unfollow system

--- app.py
+++ app.py
@@ -9,8 +9,21 @@ app.secret_key= "hi"
 
 @app.route('/')
 def index():
-    
-    return render_template("index.html",season = get_season())
+    following_list = []
+    conn= sqlite3.connect('following.db')
+    c= conn.cursor()
+
+    c.execute("SELECT * from following")
+    data = c.fetchall()
+    conn.close()
+    for i in data:
+        following_list.append(i[0])
+
+    ctx = {
+        "season" : get_season(),
+        "following_list" : following_list
+    }
+    return render_template("index.html",context=ctx)
 
 @app.route('/info/<int:id>')
 def info(id):
@@ -29,16 +42,25 @@ def info(id):
 def follow(id):
     try:
         conn = sqlite3.connect('following.db')
-        cursor = conn.cursor()
+        c = conn.cursor()
 
-        conn.execute ("CREATE TABLE IF NOT EXISTS following (anime_id INTEGER PRIMARY KEY)")
-        conn.execute("INSERT INTO  following (anime_id) VALUES (?)",(id,))
+        c.execute ("CREATE TABLE IF NOT EXISTS following (anime_id INTEGER PRIMARY KEY)")
+        c.execute("INSERT INTO  following (anime_id) VALUES (?)",(id,))
         conn.commit()
+        conn.close()
     except:
         flash("You already follow this anime.")
 
-    return redirect("/ ")
+    return redirect(f"/#{id}")
 
+@app.route('/unfollow/<int:id>')
+def unfollow(id):
+    conn= sqlite3.connect('following.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM following WHERE anime_id = ?",(id,))
+    conn.commit()
+    conn.close()
+    return redirect(f"/#{id}")
 if __name__ == "__main__":
     app.run(debug=True)
 
