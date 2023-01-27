7a452aa1403b2f5f1fdfe2ba19cde7b74e597427
tyzrex <sulavbaral58@gmail.com>
2023-01-27 21:38:21 +0545

    404.html page done and improved search and anime info page video player remains

--- app.py
+++ app.py
@@ -86,6 +86,10 @@ async def video(anime_name , ep_id):
     print(video_url) 
     return render_template("video_player.html",video_feed= video_url)
 
+# not found route 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
